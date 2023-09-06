c67635aaaa534b69d80e0a5020ac1a59fb6388d8
Egor <you@example.combesedinegor@mail.ru>
2023-09-06 22:43:42 +0300

    реализация бургер меню, реализация мобильной версии сайта, изменения в некоторых фонах и шрифтах

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,19 @@
 function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+  // Mobile nav button
+  const navBtnOpen = document.querySelector("#modalOpen");
+  const navBtnClose = document.querySelector("#modalClose");
+  const navMobileOverlay = document.querySelector("#mobileOverlay");
+  const nav = document.querySelector("#mobileNav");
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+  navBtnOpen.onclick = toggleMobileNav;
+  navBtnClose.onclick = toggleMobileNav;
+  navMobileOverlay.onclick = toggleMobileNav;
+
+  function toggleMobileNav() {
+    navMobileOverlay.classList.toggle("mobile-nav-overlay--open");
+    nav.classList.toggle("mobile-nav--open");
+    document.body.classList.toggle("no-scroll");
+  }
 }
 
 export default mobileNav;
