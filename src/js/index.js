ed99985f16346b993e3b9baf218376e23f6337b3
Egor <besedinegor@mail.ru>
2023-10-02 18:37:48 +0300

    реализация продолжения сайта, подключение datePicker, countryPicker, новый блок discover

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,29 @@
+import { easepick, TimePlugin } from "@easepick/bundle";
+import autoComplete from "@tarekraafat/autocomplete.js";
+import countryList from "./countrys/countryList.js";
 import mobileNav from "./modules/mobile-nav.js";
 mobileNav();
+
+const autoCompleteJS = new autoComplete({
+  selector: "#autoCompleteCountry",
+  placeHolder: "e.g Bali, Indonesia",
+  data: {
+    src: countryList,
+    cache: true,
+  },
+  resultItem: {
+    highlight: true,
+  },
+  events: {
+    input: {
+      selection: (event) => {
+        const selection = event.detail.selection.value;
+        autoCompleteJS.input.value = selection;
+      },
+    },
+  },
+  searchEngine: "strict",
+});
 
 // import Swiper bundle with all modules installed
 import Swiper from "swiper/bundle";
@@ -33,4 +57,12 @@ const swiper = new Swiper(".swiper", {
   scrollbar: {
     el: ".swiper-scrollbar",
   },
+});
+
+const picker = new easepick.create({
+  element: document.getElementById("datePicker"),
+  css: ["https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"],
+  format: "HH:mm, DD/MM/YY",
+  zIndex: 10,
+  plugins: [TimePlugin],
 });
